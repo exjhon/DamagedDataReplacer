ed7d99be5de331abcfa67daa728c64c94134aa81
exjhon <57669179+exjhon@users.noreply.github.com>
2024-07-15 01:14:01 +0800

    Update DamagedDataReplacerDlg.h

--- DamagedDataReplacerDlg.h
+++ DamagedDataReplacerDlg.h
@@ -1,4 +1,4 @@
-﻿
+
 // DamagedDataReplacerDlg.h: 头文件
 //
 
@@ -14,28 +14,28 @@
 // CDamagedDataReplacerDlg 对话框
 class CDamagedDataReplacerDlg : public CDialogEx
 {
-// 构造
+    // 构造
 public:
-	CDamagedDataReplacerDlg(CWnd* pParent = nullptr);	// 标准构造函数
-// 对话框数据
+    CDamagedDataReplacerDlg(CWnd* pParent = nullptr);	// 标准构造函数
+    // 对话框数据
 #ifdef AFX_DESIGN_TIME
-	enum { IDD = IDD_DAMAGEDDATAREPLACER_DIALOG };
+    enum { IDD = IDD_DAMAGEDDATAREPLACER_DIALOG };
 #endif
 
-	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
-
-
-// 实现
 protected:
-	HICON m_hIcon;
-	virtual BOOL OnInitDialog();
-	afx_msg void OnPaint();
-	afx_msg HCURSOR OnQueryDragIcon();
-	void RequestAdminPrivileges();
+    virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+    // 实现
+protected:
+    HICON m_hIcon;
+    virtual BOOL OnInitDialog();
+    afx_msg void OnPaint();
+    afx_msg HCURSOR OnQueryDragIcon();
+    void RequestAdminPrivileges();
 
     afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
-	DECLARE_MESSAGE_MAP()
+    DECLARE_MESSAGE_MAP()
 
 public:
     afx_msg void OnBnClickedButton1();
@@ -63,4 +63,6 @@ private:
     bool m_exportLog;
     std::atomic<bool> m_isComparing;
     std::thread m_compareThread;
+
+    std::vector<std::wstring> m_initialDamagedFiles; // 保存初始的损坏文件列表
 };
